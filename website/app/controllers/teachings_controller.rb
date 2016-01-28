001cb9c4a917ae7f520cf9cfdb10fdc5146091bf
rade134 <rade134@gmail.com>
2016-01-28 06:15:03 -0500

    updated website with missing and can generate data

--- website/app/controllers/teachings_controller.rb
+++ website/app/controllers/teachings_controller.rb
@@ -9,7 +9,7 @@ class TeachingsController < ApplicationController
 			fileName = "fileName"
 			out_file = File.new(fileName, "w")
 			# Get the original report, store in file
-			out_file.puts('"00R000062","2400      CT HEAD - PLAIN L3   CT HEAD CLINICAL DETAILS:  UNENHANCED AXIAL IMAGES FROM SKULL BASE TO VERTEX.  IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  CLINICAL DETAILS:  MVA ROLL OVER.  AMNESIC TO EVENT.  GCS 15 AT SCENE - LOC IN TRANSIT.   NOW 13-15, INAPPROPRIATE.  LARGE RIGHT TEMPORAL HAEMTOMA. MALOCCLUSION OF JAW.  FINDINGS:  RIGHT SIDED SCALP HAEMATOMA.  NO EXTRA-AXIAL COLLECTION.  THE GREY WHITE MATTER DIFFERENTIATION IS NORMAL.  NO HAEMORRHAGE OR MASS LESION.  NO EVIDENCE OF ACUTE OR OLD INFARCTION.  THE DILATATION OF THE VENTRICLES, SULCI AND BASAL CISTERNS ARE NORMAL FOR THE PATIENT\'S AGE.  NO FRACTURE DEMONSTRATED. CONCLUSION:  RIGHT SIDED SCALP HAEMATOMA.  OTHERWISE NORMAL STUDY. ","negative"')
+			out_file.puts('"00R000062","2400      CT HEAD - PLAIN L3   CT HEAD CLINICAL DETAILS:  UNENHANCED AXIAL IMAGES FROM SKULL BASE TO VERTEX.  IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  CLINICAL DETAILS:  MVA ROLL OVER.  AMNESIC TO EVENT.  GCS 15 AT SCENE - LOC IN TRANSIT.   NOW 13-15, INAPPROPRIATE.  LARGE RIGHT TEMPORAL HAEMTOMA. MALOCCLUSION OF JAW.  FINDINGS:  RIGHT SIDED SCALP HAEMATOMA.  NO EXTRA-AXIAL COLLECTION.  THE GREY WHITE MATTER DIFFERENTIATION IS NORMAL.  NO HAEMORRHAGE OR MASS LESION.  NO EVIDENCE OF ACUTE OR OLD INFARCTION.  THE DILATATION OF THE VENTRICLES, SULCI AND BASAL CISTERNS ARE NORMAL FOR THE PATIENT\'S AGE.  NO FRACTURE DEMONSTRATED. CONCLUSION:  RIGHT SIDED SCALP HAEMATOMA.  OTHERWISE NORMAL STUDY')
 
 			# 1) search query
 			# Get search query, stores the search query in the file
@@ -19,14 +19,20 @@ class TeachingsController < ApplicationController
 			# done close the file
 			out_file.close
 
-			resultTemp = %x(python search_engine.py fileName)
+			resultTemp = %x(python similarity.py fileName)
 			
 			# we should process resultCSV so that it can be displayed correctly
 			resultTemp = resultTemp.split("\n")
 			@result = []	
 			resultTemp.each do |i|
 				temp = i.split("\t")
-				@result.push([temp[0],temp[1],temp[2]])
+				if temp[0] == "n"	
+					@result.push(["n",temp[1]])
+				elsif temp[0] == "c"
+					@result.push(["c",temp[1],temp[2]])
+				elsif temp[0] == "m"
+					@result.push(["m",temp[1]])
+				end
 			end	
 			# Then we deleted the fileName file, because the search_engine program finished using it
 			#File.delete(fileName)
