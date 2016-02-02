22bd14e814e23ddc5f2a62799bb8bb9800c15aa5
wgale <william@margale.net>
2016-02-03 08:50:27 +1030

    Sentence vector bug fixed so that they are now useful, obsolete functions removed and extra comparison and similarity functions added

--- driver.py
+++ driver.py
@@ -11,8 +11,8 @@ import rnn
 # modelGeneration.buildWord2VecModel()
 # modelGeneration.buildDoc2VecModel()
 # modelGeneration.testClassification()
-# searchTerm = "haem"
-searchTerm = "left sided embolus"
+searchTerm = "haemorrhage"
+# searchTerm = "left sided embolus"
 # searchTerm = "2400      CT HEAD - PLAIN L3  CT HEAD:  CLINICAL DETAILS:  INVOLVED IN FIGHT, KICKED IN HIS HEAD, VOMITED AFTER THIS WITH EPISODIC STARING EPISODES WITH TEETH GRINDING. ALSO INTOXICATED (BREATH ALCOHOL ONLY 0.06). PROCEDURE:  PLAIN SCANS THROUGH THE BRAIN FROM SKULL BASE TO NEAR VERTEX. IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  REPORT:  VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS SYMMETRICAL AROUND THE MIDLINE.  NORMAL GREY/WHITE DIFFERENTIATION.  NO INTRACEREBRAL HAEMATOMA OR EXTRA AXIAL COLLECTION. NO CRANIAL VAULT FRACTURE SEEN.  COMMENT: STUDY WITHIN NORMAL LIMITS."
 # searchTerm = "GREY/WHITE MATTER DIFFERENTIATION"
 # search.searchEngineTest("rnn",searchTerm)
@@ -20,16 +20,12 @@ searchTerm = "left sided embolus"
 # generateReports.labelClassification()
 # generateReports.labelClassificationD2V()
 
-# preprocess.preprocessReports()
-# rnn.buildWord2Vec()
-# rnn.buildRNN()
-# rnn.fullToEncoder()
-# rnn.buildPredictionsRNN()
-
 # rnn.preprocessReports()
 # rnn.buildWord2VecSentences()
-rnn.testWord2VecModel()
+# rnn.testWord2VecModel()
 # rnn.buildSentenceRNN()
 # rnn.sentenceToEncoder()
 # rnn.sentencesToDense()
 # rnn.searchRNN(searchTerm)
+# rnn.nextWords("VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS")
+rnn.compareSentences("There is a intracranial haemorrhage.","There is a haemorrhage in the cranium.")
