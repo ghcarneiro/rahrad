ebc2a2d81750a5151636a44dbba2aabea651b873
wgale <william@margale.net>
2016-02-08 18:39:40 +1030

    RNN Report Model added and PR curves generated (outdated doc2vec in curves)

--- driver.py
+++ driver.py
@@ -16,7 +16,7 @@ import rnn
 # searchTerm = "2400      CT HEAD - PLAIN L3  CT HEAD:  CLINICAL DETAILS:  INVOLVED IN FIGHT, KICKED IN HIS HEAD, VOMITED AFTER THIS WITH EPISODIC STARING EPISODES WITH TEETH GRINDING. ALSO INTOXICATED (BREATH ALCOHOL ONLY 0.06). PROCEDURE:  PLAIN SCANS THROUGH THE BRAIN FROM SKULL BASE TO NEAR VERTEX. IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  REPORT:  VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS SYMMETRICAL AROUND THE MIDLINE.  NORMAL GREY/WHITE DIFFERENTIATION.  NO INTRACEREBRAL HAEMATOMA OR EXTRA AXIAL COLLECTION. NO CRANIAL VAULT FRACTURE SEEN.  COMMENT: STUDY WITHIN NORMAL LIMITS."
 # searchTerm = "GREY/WHITE MATTER DIFFERENTIATION"
 # search.searchEngineTest("rnn",searchTerm)
-# generateReports.precisionRecall("pr_tests.csv")
+generateReports.precisionRecall("pr_tests.csv")
 # generateReports.labelClassification()
 # generateReports.labelClassificationD2V()
 
@@ -35,7 +35,7 @@ import rnn
 # rnn.nextWords("left sided embolus")
 
 
-rnn.reportsToDense()
-rnn.buildReportRNN()
-rnn.reportToEncoder()
-rnn.reports2vecs()
+# rnn.reportsToDense()
+# rnn.buildReportRNN()
+# rnn.reportToEncoder()
+# rnn.reports2vecs()
