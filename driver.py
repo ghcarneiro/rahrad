b555495c7b9a70c3e528caaef835d16dd5a0c65c
wgale <william@margale.net>
2016-02-11 15:16:26 +1030

    RNN Model at Report and Sentence level bug fixed and run with PR curves generated

--- driver.py
+++ driver.py
@@ -12,11 +12,12 @@ import rnn
 # modelGeneration.buildDoc2VecModel()
 # modelGeneration.testClassification()
 # searchTerm = "haemorrhage"
+# searchTerm = "no haemorrhage"
 # searchTerm = "left sided embolus"
 # searchTerm = "2400      CT HEAD - PLAIN L3  CT HEAD:  CLINICAL DETAILS:  INVOLVED IN FIGHT, KICKED IN HIS HEAD, VOMITED AFTER THIS WITH EPISODIC STARING EPISODES WITH TEETH GRINDING. ALSO INTOXICATED (BREATH ALCOHOL ONLY 0.06). PROCEDURE:  PLAIN SCANS THROUGH THE BRAIN FROM SKULL BASE TO NEAR VERTEX. IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  REPORT:  VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS SYMMETRICAL AROUND THE MIDLINE.  NORMAL GREY/WHITE DIFFERENTIATION.  NO INTRACEREBRAL HAEMATOMA OR EXTRA AXIAL COLLECTION. NO CRANIAL VAULT FRACTURE SEEN.  COMMENT: STUDY WITHIN NORMAL LIMITS."
 # searchTerm = "GREY/WHITE MATTER DIFFERENTIATION"
 # search.searchEngineTest("rnn",searchTerm)
-# generateReports.precisionRecall("pr_tests.csv")
+generateReports.precisionRecall("pr_tests.csv")
 # generateReports.labelClassification()
 # generateReports.labelClassificationD2V()
 
@@ -24,19 +25,21 @@ import rnn
 # rnn.buildWord2VecSentences()
 # rnn.testWord2VecModel()
 
-# rnn.buildSentenceRNN()
-# rnn.buildSentenceRNN(epochs=1,continueTraining=True)
+# rnn.buildSentenceRNN(epochs=15)
+# rnn.buildSentenceRNN(epochs=20,continueTraining=True)
 # rnn.sentenceToEncoder()
 # rnn.sentencesToDense()
 
-# rnn.searchRNN(searchTerm)
+# rnn.searchRNN("haemorrhage")
+# rnn.searchRNN("no haemorrhage")
+# rnn.searchRNN("left sided embolus")
 
-# rnn.compareSentences("There is a intracranial haemorrhage.","There is a haemorrhage in the cranium.")
 # rnn.compareSentences("There is a intracranial haemorrhage","There is a haemorrhage in the cranium")
-# rnn.compareSentences("There is no intracranial haemorrhage.","There is a haemorrhage in the cranium.")
 # rnn.compareSentences("There is no intracranial haemorrhage","There is a haemorrhage in the cranium")
-# rnn.compareSentences("There is a intracranial haemorrhage.","The study is within normal limits.")
 # rnn.compareSentences("There is a intracranial haemorrhage","The study is within normal limits")
+# rnn.compareSentences("There is a intracranial haemorrhage.","There is a haemorrhage in the cranium.")
+# rnn.compareSentences("There is no intracranial haemorrhage.","There is a haemorrhage in the cranium.")
+# rnn.compareSentences("There is a intracranial haemorrhage.","The study is within normal limits.")
 
 # rnn.nextWords("VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS")
 # rnn.nextWords("VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE")
@@ -45,6 +48,7 @@ import rnn
 
 
 # rnn.reportsToDense()
-# rnn.buildReportRNN()
+# rnn.buildReportRNN(epochs=50)
+# rnn.buildReportRNN(epochs=10,continueTraining=True)
 # rnn.reportToEncoder()
 # rnn.reports2vecs()
