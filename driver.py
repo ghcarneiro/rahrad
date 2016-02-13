20b6496745631c961909a246bcdd3e28339f374e
wgale <william@margale.net>
2016-02-13 11:31:10 +1030

    Report level RNN trained until convergence, new pr curves added

--- driver.py
+++ driver.py
@@ -17,7 +17,7 @@ import rnn
 # searchTerm = "2400      CT HEAD - PLAIN L3  CT HEAD:  CLINICAL DETAILS:  INVOLVED IN FIGHT, KICKED IN HIS HEAD, VOMITED AFTER THIS WITH EPISODIC STARING EPISODES WITH TEETH GRINDING. ALSO INTOXICATED (BREATH ALCOHOL ONLY 0.06). PROCEDURE:  PLAIN SCANS THROUGH THE BRAIN FROM SKULL BASE TO NEAR VERTEX. IMAGES PHOTOGRAPHED ON SOFT TISSUE AND BONE WINDOWS.  REPORT:  VENTRICULAR CALIBRE IS WITHIN NORMAL LIMITS FOR AGE AND IT IS SYMMETRICAL AROUND THE MIDLINE.  NORMAL GREY/WHITE DIFFERENTIATION.  NO INTRACEREBRAL HAEMATOMA OR EXTRA AXIAL COLLECTION. NO CRANIAL VAULT FRACTURE SEEN.  COMMENT: STUDY WITHIN NORMAL LIMITS."
 # searchTerm = "GREY/WHITE MATTER DIFFERENTIATION"
 # search.searchEngineTest("rnn",searchTerm)
-generateReports.precisionRecall("pr_tests.csv")
+# generateReports.precisionRecall("pr_tests.csv")
 # generateReports.labelClassification()
 # generateReports.labelClassificationD2V()
 
@@ -48,7 +48,7 @@ generateReports.precisionRecall("pr_tests.csv")
 
 
 # rnn.reportsToDense()
-# rnn.buildReportRNN(epochs=50)
-# rnn.buildReportRNN(epochs=10,continueTraining=True)
+# rnn.buildReportRNN(epochs=15)
+# rnn.buildReportRNN(epochs=50,continueTraining=True)
 # rnn.reportToEncoder()
 # rnn.reports2vecs()
