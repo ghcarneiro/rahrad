8fc492f6ef95dadb6ba4ff8b73a6b73572554f2c
jackgerrits <jackgerrits95@gmail.com>
2016-04-26 21:59:19 +0930

    Added initial implementation of automatic learning

---
+++ sentence_classification/automaticLearning.py
@@ -0,0 +1,78 @@
+import sys, numpy as np
+from dataUtils import *
+import pipelines
+from random import shuffle
+
+# Will only tag if the confidence is above these values
+POS_THRESHOLD = 0.9
+NEG_THRESHOLD = 0.9
+
+usage = "USAGE: " + sys.argv[0] + " passes inputFile outputFile"
+if len(sys.argv) != 4:
+    print usage
+    sys.exit(1)
+
+passes = int(float(sys.argv[1]))
+inputFile = sys.argv[2]
+outputFile = sys.argv[3]
+
+data = readFromCSV(inputFile)[:1000]
+
+newPos = 0
+newNeg = 0
+
+# Each pass contains: classifier training, label prediction, finding the max and placing the respective labels
+for i in xrange(passes):
+    print ""
+    print "PASS " + str(i)
+
+    # Extract lists to be used for model and classifier training
+    sentences = [x.processedSentence for x in data if x.diagTag != "" and x.diagTag != "u"]
+    labels = [np.float32(x.diagTag == "p") for x in data if x.diagTag != "" and x.diagTag != "u"]
+
+    print "There are " + str(len(sentences)) + " tagged sentences in this pass"
+
+    # Train model and classifier
+    pipe = pipelines.get_count_lsi_randomforest()
+    pipe.fit(sentences, labels)
+
+    bestPos = 0
+    bestNeg = 0
+    newPosThisPass = 0
+    newNegThisPass = 0
+
+    # Makes predicitions for the unlabelled data in the first 1000 items,
+    # also keeps track of the max confidence of each classification
+    for item in [item for item in data[:1000] if item.diagTag == ""]:
+        item.diagProbs = pipe.predict_proba([item.processedSentence])[0]
+        bestNeg = max(item.diagProbs[0], bestNeg)
+        bestPos = max(item.diagProbs[1], bestPos)
+
+    print "Best positive confidence: " + str(bestPos)
+    print "Best negative confidence: " + str(bestNeg)
+
+    # Allocates the labels for each of the determined max values if they lie above the threshold
+    for item in [item for item in data[:1000] if item.diagTag == ""]:
+        if POS_THRESHOLD < bestPos == item.diagProbs[1]:
+            item.diagTag = "p"
+            newPosThisPass += 1
+        if NEG_THRESHOLD < bestNeg == item.diagProbs[0]:
+            item.diagTag = "n"
+            newNegThisPass += 1
+
+    # Shuffle the data to ensure a new set of 1000 is served up on the next pass
+    shuffle(data)
+
+    newPos += newPosThisPass
+    newNeg += newNegThisPass
+
+    print str(newPosThisPass) + " new positive tags added this pass"
+    print str(newNegThisPass) + " new negative tags added this pass"
+
+# Save the ouput of the automatic learning
+writeToCSV(outputFile, data)
+
+print ""
+print "Completed " + str(passes) + " passes over the data."
+print str(newPos) + " new positive tags added"
+print str(newNeg) + " new negative tags added"
