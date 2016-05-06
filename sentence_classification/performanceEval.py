1ffa392e40e754f262c4258782f061ddff409ead
jackgerrits <jackgerrits95@gmail.com>
2016-05-06 10:31:20 +0930

    Added training set performanmce data display

--- sentence_classification/performanceEval.py
+++ sentence_classification/performanceEval.py
@@ -6,7 +6,34 @@ import pipelines
 from dataUtils import readFromCSV
 import numpy as np
 
-usage = "USAGE: " + sys.argv[0] + "type numTrain dataFile"
+
+def addCurves(setType, y_true, y_pos_score, subplotOffset, rows=2, cols=2):
+    false_positive_rate, true_positive_rate, thresholds_roc = roc_curve(y_true, y_pos_score)
+    roc_auc = auc(false_positive_rate, true_positive_rate)
+    pr_avg = average_precision_score(y_true, y_pos_score)
+    precision, recall, thresholds_pr = precision_recall_curve(y_true, y_pos_score)
+
+    plt.subplot(rows, cols, subplotOffset)
+    plt.title(setType + ' - Receiver Operating Characteristic')
+    plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([-0.1, 1.2])
+    plt.ylim([-0.1, 1.2])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+
+    plt.subplot(rows, cols, subplotOffset + 1)
+    plt.title(setType + ' - Precision Recall')
+    plt.plot(recall, precision, 'b', label='APS = %0.2f' % pr_avg)
+    plt.legend(loc='lower right')
+    plt.xlim([-0.1, 1.2])
+    plt.ylim([-0.1, 1.2])
+    plt.ylabel('Precision')
+    plt.xlabel('Recall')
+
+
+usage = "USAGE: " + sys.argv[0] + " type numTrain dataFile [model file]"
 if len(sys.argv) != 4:
     print usage
     sys.exit(1)
@@ -32,10 +59,7 @@ trainReportIDs = [x.reportID for x in filteredData[:numTrain]]
 trainLabels = labels[:numTrain]
 
 # Create transformation pipeline
-# testPipe = pipelines.get_count_randomforest()
 testPipe = pipelines.get_count_lsi_randomforest()
-# testPipe = pipelines.get_tfidf_lsi_randomforest()
-# testPipe = pipelines.get_count_lsi_SVM()
 testPipe.fit(train, trainLabels)
 
 test = []
@@ -51,42 +75,18 @@ print "Total = " + str(len(filteredData)) + " [" + str(labels.count(0)) + ", " +
 print "Train = " + str(len(train)) + " [" + str(trainLabels.count(0)) + ", " + str(trainLabels.count(1)) + "]"
 print "Test = " + str(len(test)) + " [" + str(testLabels.count(0)) + ", " + str(testLabels.count(1)) + "]"
 
-y_true = testLabels
-y_pred = [testPipe.predict([x]).tolist()[0] for x in test]
-y_pos_score = [testPipe.predict_proba([x]).tolist()[0][1] for x in test]
+# Training performance data
+y_true_train = trainLabels
+y_pos_score_train = [testPipe.predict_proba([x]).tolist()[0][1] for x in train]
 
-print y_true
-print y_pred
+# Testing performance data
+y_true_test = testLabels
+y_pred_test = [testPipe.predict([x]).tolist()[0] for x in test]
+y_pos_score_test = [testPipe.predict_proba([x]).tolist()[0][1] for x in test]
+print classification_report(y_true_test, y_pred_test, target_names=['yes', 'no'])
 
-print classification_report(y_true, y_pred, target_names=['yes', 'no'])
-
-false_positive_rate, true_positive_rate, thresholds_roc = roc_curve(y_true, y_pos_score)
-roc_auc = auc(false_positive_rate, true_positive_rate)
-
-pr_avg = average_precision_score(y_true, y_pos_score)
-
-precision, recall, thresholds_pr = precision_recall_curve(y_true, y_pos_score)
-
+# Plot both sets of perfomance curves
 plt.figure()
-
-# Plot ROC curve
-plt.subplot(121)
-plt.title('Receiver Operating Characteristic')
-plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.2f' % roc_auc)
-plt.legend(loc='lower right')
-plt.plot([0, 1], [0, 1], 'r--')
-plt.xlim([-0.1, 1.2])
-plt.ylim([-0.1, 1.2])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-
-plt.subplot(122)
-plt.title('Precision Recall')
-plt.plot(recall, precision, 'b', label='APS = %0.2f' % pr_avg)
-plt.legend(loc='lower right')
-plt.xlim([-0.1, 1.2])
-plt.ylim([-0.1, 1.2])
-plt.ylabel('Precision')
-plt.xlabel('Recall')
-
+addCurves("Train", y_true_train, y_pos_score_train, 1)
+addCurves("Test", y_true_test, y_pos_score_test, 3)
 plt.show()
